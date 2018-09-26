108285f454bce900e62cb979a6bf65dd7c5d54f9
xiejunjie629 <40513328+xiejunjie629@users.noreply.github.com>
2018-09-26 21:15:03 +0800

    Add files via upload

---
+++ practice5/src/employee/TestEmployee.java
@@ -0,0 +1,16 @@
+package employee;
+import java.util.*;
+public class TestEmployee {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Employee {
+	String name;
+	int age;
+	String position;
+	double salary;
+}
